b65ed423073e92f7694771bd557f0f7274321c2f
Tomasz Koziarek <tomek20081@outlook.com>
2023-02-01 06:31:04 +0100

    praca domowa 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,20 +1,20 @@
 ﻿string name = "Ewa";
 var gender = "Kobieta";
-int age = 18;
+int age = 16;
 
-if(age < 30 && gender =="Kobieta")
+if(age < 30 && gender == "Kobieta")
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if(gender == "Kobieta"  && name == "Ewa")
+else if(gender == "Kobieta" && name == "Ewa" && age == 33)
 {
     Console.WriteLine("Ewa, lat 33");
 }
-else if(name == "Tomek" && age > 33)
+else if(name == "Tomek" && age < 17)
 {
     Console.WriteLine("Niepełnoletni Mężczyzna");
 }
 else
 {
-    Console.WriteLine("Ta kobieta ma na imie Ewa");
+    Console.WriteLine("Ta kobieta ma na imie Maria");
 }
